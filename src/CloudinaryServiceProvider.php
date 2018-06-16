97c49ec09133a3eddfbdfe2166e9608afd298c50
adamtorok <adamtorok@sch.bme.hu>
2018-06-16 13:55:15 +0200

    set up

--- src/CloudinaryServiceProvider.php
+++ src/CloudinaryServiceProvider.php
@@ -1,7 +1,42 @@
 <?php
 
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\ServiceProvider;
 
-class CloudinaryServiceProvider
+use Enl\Flysystem\Cloudinary\ApiFacade as CloudinaryClient;
+use Enl\Flysystem\Cloudinary\CloudinaryAdapter;
+
+use League\Flysystem\Filesystem;
+
+class CloudinaryServiceProvider extends ServiceProvider
 {
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/config.php' => config_path('cloudinary.php'),
+        ]);
 
+        Storage::extend('cloudinary', function ($app, $config) {
+            $client = new CloudinaryClient($config['cloudinary']);
+
+            return new Filesystem(new CloudinaryAdapter($client));
+        });
+    }
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/config/config.php', 'cloudinary'
+        );
+    }
 }
---
+++ src/config/cloudinary.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    /**
+     * Cloudinary cloud name
+     */
+    'cloud_name' => '',
+
+    /**
+     * Api key
+     */
+    'api_key' => '',
+
+    /**
+     * Api secret
+     */
+    'api_secret' => '',
+
+    /**
+     * Set this to true if you want to overwrite existing files using $filesystem->write();
+     */
+    'overwrite' => true
+];
