ffab7a37b5c520364dd8dc9b57e15e06604e7536
adamtorok <adamtorok@sch.bme.hu>
2018-06-16 14:09:53 +0200

    config from env

--- src/config/cloudinary.php
+++ src/config/cloudinary.php
@@ -4,20 +4,20 @@ return [
     /**
      * Cloudinary cloud name
      */
-    'cloud_name' => '',
+    'cloud_name' => env('CLOUDINARY_CLOUD'),
 
     /**
      * Api key
      */
-    'api_key' => '',
+    'api_key' => env('CLOUDINARY_API_KEY'),
 
     /**
      * Api secret
      */
-    'api_secret' => '',
+    'api_secret' => env('CLOUDINARY_API_SECRET'),
 
     /**
      * Set this to true if you want to overwrite existing files using $filesystem->write();
      */
-    'overwrite' => true
+    'overwrite' => env('CLOUDINARY_OVERWRITE', true)
 ];
